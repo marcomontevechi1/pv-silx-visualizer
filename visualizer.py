e2d2789d323b4078206044a5b0fdc959c96c2cbb
Marco A. B. Montevechi Filho <marco.filho@lnls.br>
2024-02-16 11:36:50 -0300

    Add PVPlotter. Remove imports and classes local to createWindow.
    To use RestoreAction inside PVPlotter, i removed it from createWindow function,
    along with some imports.
    
    The original commented code mentioned the need for some local imports because
    some libraries needed local configuration before being used, but after testing
    our use cases I came to the conclusion that this is not needed. I could be proven
    wrong in the long term.
    
    PVPlotter is just a Plot2D with callbacks to update PVs and with the
    restore action.
    
    I wanted PVPlotter.pv_replot() to get height and width directly from
    self.height.value and self.width.value, but for some reason this didnt work out
    as any get operation inside the callback timed out. In special corner cases,
    this can obviously lead to synchronization problems. I am assuming Array shapes
    wont change often enough to trigger this mess. Or at least not frequently enough to justify
    a different approach (YET).

--- visualizer.py
+++ visualizer.py
@@ -1,76 +1,120 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 import numpy
+from os import getenv
 
+from silx.gui.plot import Plot2D
+from silx.gui.plot.actions import PlotAction
 from silx.gui import qt
 from dotenv import load_dotenv
-from os import getenv
+import epics
+
+
+class RestoreAction(PlotAction):
+    def __init__(self, plot, parent=None):
+
+        restore = qt.QIcon("./matrix.png")
+
+        super(RestoreAction, self).__init__(
+            plot, icon=restore, text='Restore',
+            tooltip='Geometrically restore PiMega image',
+            triggered=self.__store_variable,
+            checkable=True, parent=parent)
+
+        plot.sigActiveImageChanged.connect(self.__restore)
+        self.restore = False
+        self.prev_data = None
+
+    def __store_variable(self, checked):
+        '''
+        Variable to tell if its supposed to restore each new
+        image.
+        '''
+        self.restore = checked
+        self.__restore()
+
+        if not checked:
+            self.plot.replot()
+
+    def __restore(self):
+        '''
+        Restore current image.
+        '''
+
+        activeImage = self.plot.getActiveImage()
+
+        if activeImage is not None:
+
+            data = activeImage.getData()
+            self.plot.sigActiveImageChanged.disconnect(self.__restore)
+
+            if self.restore:
+
+                self.prev_data = activeImage.getData()
+                self.originalData = activeImage.getData()
+                data += 500
+                activeImage.setData(data)
+
+            elif self.prev_data is not None:
+                activeImage.setData(self.prev_data)
+                self.prev_data = None
+
+            self.plot.sigActiveImageChanged.connect(self.__restore)
+
+
+class PVPlotter(Plot2D):
+
+    def __init__(self):
+        super(PVPlotter, self).__init__()
+
+        self.create_pvs()
+
+    def create_pvs(self):
+
+        self.array_pv = epics.PV(getenv("EPICS_ARRAY_PV"), auto_monitor=True)
+        self.width_pv = epics.PV(
+            getenv("EPICS_ARRAY_WIDTH"), auto_monitor=True)
+        self.height_pv = epics.PV(
+            getenv("EPICS_ARRAY_HEIGHT"), auto_monitor=True)
+        self.width_val = self.width_pv.value
+        self.height_val = self.height_pv.value
+
+        self.setWindowTitle(self.array_pv.pvname)
+        action = RestoreAction(self, self)
+        self.profile.addAction(action)
+
+        self.array_pv.add_callback(self.pv_replot)
+        self.pv_replot(value=self.array_pv.value)
+
+    def update_dimensions(self, *args, **kwargs):
+        pvname = kwargs["pvname"]
+        value = kwargs["value"]
+        if pvname == self.height_pv.pvname:
+            self.height_val = value
+        elif pvname == self.width_pv.pvname:
+            self.width_val = value
+
+    def pv_replot(self, *args, **kwargs):
+        if kwargs["value"] is not None:
+
+            data = numpy.reshape(
+                kwargs["value"], (self.height_val, self.width_val))
+            self.addImage(data)
+
 
 def createWindow(parent, settings):
     # Local import to avoid early import (like h5py)
-    #SOme libraries have to be configured first properly
-    from silx.gui.plot.actions import PlotAction
-    from silx.gui import qt
+    # SOme libraries have to be configured first properly
     from silx.app.view.Viewer import Viewer
     from silx.app.view.ApplicationContext import ApplicationContext
-        
-    class RestoreAction(PlotAction):
-        def __init__(self, plot, parent=None):
-            
-            restore = qt.QIcon("./matrix.png")
-            
-            super(RestoreAction, self).__init__(
-                plot, icon=restore, text='Restore',
-                tooltip='Geometrically restore PiMega image',
-                triggered=self.__store_variable,
-                checkable=True, parent=parent)
-            
-            plot.sigActiveImageChanged.connect(self.__restore)
-            self.restore = False
-            self.prev_data = None
-            
-        def __store_variable(self, checked):
-            '''
-            Variable to tell if its supposed to restore each new
-            image.
-            '''
-            self.restore = checked
-            self.__restore()
-            
-            if not checked:
-                self.plot.replot()
 
-        def __restore(self):
-            '''
-            Restore current image.
-            '''
-
-            activeImage = self.plot.getActiveImage()
-
-            if activeImage is not None:
-                    
-                data = activeImage.getData()
-                self.plot.sigActiveImageChanged.disconnect(self.__restore)
-                
-                if self.restore:
-                    
-                    self.prev_data = activeImage.getData()
-                    self.originalData = activeImage.getData()
-                    data+=500
-                    activeImage.setData(data)
-                    
-                elif self.prev_data is not None:
-                    activeImage.setData(self.prev_data)
-                    self.prev_data = None
-                    
-                self.plot.sigActiveImageChanged.connect(self.__restore)
-           
     class MyApplicationContext(ApplicationContext):
         """This class is shared to all the silx view application."""
+
         def __init__(self, parent, settings=None):
             super(MyApplicationContext, self).__init__(parent, settings)
-    
+
         def findPrintToolBar(self, plot):
             # FIXME: It would be better to use the Qt API
             return plot._outputToolBar
@@ -87,12 +131,30 @@ def createWindow(parent, settings):
                 toolBar.addAction(action)
 
     class MyViewer(Viewer):
-        
+
         def __init__(self, parent=None, settings=None):
             super(MyViewer, self).__init__(parent=None, settings=None)
 
         def plotPv(self):
-            print("Keep working here")
+
+            load_dotenv()
+
+            if None in [getenv("EPICS_ARRAY_PV"),
+                        getenv("EPICS_ARRAY_WIDTH"),
+                        getenv("EPICS_ARRAY_HEIGHT")]:
+                msg = qt.QMessageBox()
+                msg_ = "Array PVs are misconfigured. Correct .env file or set the"
+                msg_ += " environment variables before running this application."
+                msg_ += " Desired variables are EPICS_ARRAY_PV, EPICS_ARRAY_WIDTH"
+                msg_ += " and EPICS_ARRAY_HEGHT."
+                msg.setText(msg_)
+                msg.setIcon(qt.QMessageBox.Critical)
+                msg.setWindowTitle("PVs not defined.")
+                msg.exec()
+                return
+
+            self.PvPlot = PVPlotter()
+            self.PvPlot.show()
 
         def createActions(self):
             super(MyViewer, self).createActions()
@@ -109,7 +171,7 @@ def createWindow(parent, settings):
             fileMenu.addSeparator()
             fileMenu.addAction(self._exitAction)
             fileMenu.aboutToShow.connect(self._Viewer__updateFileMenu)
-            
+
             pvMenu = self.menuBar().addMenu("&Plot PV")
             pvMenu.addAction(self._plotPvAction)
 
